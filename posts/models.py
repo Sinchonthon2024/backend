6dda515b1f8d57941082e8dfbf2f20c1138293df
YUNA329 <ynmerry0329@gmail.com>
2024-08-24 02:27:50 +0900

    fix:model

--- posts/models.py
+++ posts/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     link = models.CharField(max_length=255, blank=True, null=True) 
     deadline = models.DateTimeField()
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
